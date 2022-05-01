58a081380014b29b660e7eea8e09f96690500d29
Shayan Hosseini <shayan.hosseiny@gmail.com>
2022-04-30 21:55:44 -0700

    add FromIter for gomaps

--- gomaps/gomaps.go
+++ gomaps/gomaps.go
@@ -1,8 +1,21 @@
 package gomaps
 
+import (
+	"github.com/shayanh/gcl"
+	"github.com/shayanh/gcl/iters"
+)
+
 func Iter[M ~map[K]V, K comparable, V any](m M) *Iterator[K, V] {
 	return &Iterator[K, V]{
 		m:     m,
 		index: -1,
 	}
+}
+
+func FromIter[K comparable, V any](it iters.Iterator[gcl.MapElem[K, V]]) (res map[K]V) {
+	for it.HasNext() {
+		elem := it.Next()
+		res[elem.Key] = elem.Value
+	}
+	return
 }
