321dbac2adce594ab2be79657a7e5d0a9ffd758a
Shayan Hosseini <shayan.hosseiny@gmail.com>
2022-04-30 18:10:56 -0700

    more goslices methods

--- goslices/goslices.go
+++ goslices/goslices.go
@@ -26,6 +26,26 @@ func FromIter[T any](it iters.Iterator[T]) (res []T) {
 	return
 }
 
+func PushFront[S ~[]T, T any](s S, elems ...T) S {
+	return append(elems, s...)
+}
+
+func PopBack[S ~[]T, T any](s S) S {
+	return s[0 : len(s)-1]
+}
+
+func PopFront[S ~[]T, T any](s S) S {
+	return s[1:]
+}
+
+func Front[S ~[]T, T any](s S) T {
+	return s[0]
+}
+
+func Back[S ~[]T, T any](s S) T {
+	return s[len(s)-1]
+}
+
 func Reverse[S ~[]T, T any](s S) {
 	internal.Reverse[T](Iter(s), RIter(s), len(s))
 }
